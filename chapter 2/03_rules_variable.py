05c357a26c00389fa03367d3be30e957201c98f5
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-15 21:29:42 +0530

    kuch bhi likho yahan

--- chapter 2/03_rules_variable.py
+++ chapter 2/03_rules_variable.py
@@ -1,9 +1,6 @@
 
- a = 23
-
+a = 23
 aaa = 432
-
 harry = 43
-
 sameer = 43
- #@anshpandey96
+#@anshpandey96
---
+++ chapter 2/04_operator.py
@@ -0,0 +1,40 @@
+
+a = 7
+b = 4
+
+c = a + b
+print(c)
+# Assignment operator
+a = 4 - 2
+print(a)
+b = 6
+b += 4
+print(b)
+
+# Comparison operator
+d = 5 < 4
+
+print(d)
+
+a = 5
+b = 5
+
+print(a == b)
+
+e = True or False
+
+# Truth tanle of 'or'
+
+print("True or False is " , True or False )
+print("True or False is " , True or False )
+print("False or True is " , False or True )
+
+#Truth table of 'and'
+
+print("True or False is " , True and False )
+print("True and False is " , True and False )
+print("False or True is " , False and True )
+print("False or True is " , False and True )
+
+print(not(True))
+
