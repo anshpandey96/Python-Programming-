038bd72f8ed26d9c43a5776dfa5d512218f30572
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-17 20:23:49 +0530

    Intro  to String

--- chapter 2/chapter 2 practice/06_problem6.py
+++ chapter 2/chapter 2 practice/06_problem6.py
@@ -3,6 +3,7 @@ a = int ( input ("Enter the first number: "))
 
 # b = int ( input ("Enter the second number: "))
 
+# print("The square of the number is : ", a^2) # Incorrect for findinng quare of a  number
 
-print ("The fourth power of the number is : ", a**2)
+print("The fourth power of the number is : ", a**2)
 
