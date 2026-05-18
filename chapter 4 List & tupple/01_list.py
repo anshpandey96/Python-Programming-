f7c125ea3427c9f1a4aa01f573adafa70788ca0b
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-18 16:37:47 +0530

    List in Python

---
+++ chapter 4 List & tupple/01_list.py
@@ -0,0 +1,21 @@
+friends = ["ansh", "sakshi", "atul", "ashu", 5, False, "akash", "rohan", "shivam", "pratik"]
+
+print(friends)
+
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+print(friends[4])
+print(friends[5])
+print(friends[6])
+print(friends[7])
+print(friends[8])
+print(friends[9])
+print(friends[-1])
+
+friends[0] = "anshika"
+print(friends[0])  # we can change the value of list but not the string
+
+print(friends[1:4])  # it will print the value from index 1 to 3 not 4
+print(friends[1:])  # it will print the value from index 1 to  git
