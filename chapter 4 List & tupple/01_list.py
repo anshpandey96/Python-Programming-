89f3a45a54d485869e439a84c8b23179c276957c
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-18 16:59:47 +0530

    introduction to list in python

--- chapter 4 List & tupple/01_list.py
+++ chapter 4 List & tupple/01_list.py
@@ -18,4 +18,42 @@ friends[0] = "anshika"
 print(friends[0])  # we can change the value of list but not the string
 
 print(friends[1:4])  # it will print the value from index 1 to 3 not 4
-print(friends[1:])  # it will print the value from index 1 to  git
+print(friends[1:])   # it will print the value from index 1 to end
+
+l1 = [1, 34, 2, 2, 5, 7, 93, 45, 11, 56]
+# l1.sort()
+# l1.reverse()
+l1.insert(3,333333)# Insert 33333 suchthat itsw index in the list is 3
+# print(l1)
+l1.append(333333) # Insert 33333 at the end of the list
+print(l1.pop(3))
+print(11)
+friends = ["ansh", "sakshi", "atul", "ashu", 5, False, "akash", "rohan", "shivam", "pratik"]
+
+print(friends)
+
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+print(friends[4])
+print(friends[5])
+print(friends[6])
+print(friends[7])
+print(friends[8])
+print(friends[9])
+print(friends[-1])
+
+friends[0] = "anshika"
+print(friends[0])  # we can change the value of list but not the string
+
+print(friends[1:4])  # it will print the value from index 1 to 3 not 4
+print(friends[1:])   # it will print the value from index 1 to end
+
+l1 = [1, 34, 62, 2, 5, 7, 93, 45, 11, 56]
+# l1.sort()
+# l1.reverse()
+l1.insert(3,333333)# Insert 33333 suchthat itsw index in the list is 3
+# print(l1)
+
+
