8a8c115337e48165655fd7d3d08d8ceb4613c4a3
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-19 14:56:27 +0530

    Added code for tuple data structure

---
+++ chapter 4 List & tupple/02_list_methods.py
@@ -0,0 +1,12 @@
+friends = ["ansh", "sakshi", "atul", "ashu", 5, False, "pratik"]
+print(friends)
+friends.append("akash") # Insert "akash" at the end of the list
+print(friends)
+
+l1 = [1, 34, 62, 2, 5, 7, 93, 45, 11, 56]
+l1.sort() # Sort the list in ascending order
+print(l1)
+l1.reverse() # Reverse the order of the list
+print(l1)
+l1.insert(3,333333) # Insert 333333 such that its index in the list is 3
+print(l1)
---
+++ chapter 4 List & tupple/03_tuple.py
@@ -0,0 +1,8 @@
+a = (1, 2, 45, 532, False, "Rohan", "shivam")
+
+print(type(a))
+
+# tupple can not be modified but list can be modified
+
+# a[0] = 100 # this will give error because tuple is immutable
+
