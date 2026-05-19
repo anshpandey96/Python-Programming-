3799ce90373a8af229c7ac4ee993e1d43d640f6e
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-19 22:00:39 +0530

    Added code for problem 3 to input 6 fruit names and display them as a tuple

---
+++ chapter 4 Problem set/03_problem3.py
@@ -0,0 +1,19 @@
+# wap to input 6 student to show marks of 6 students in a sorted order
+fruits = []
+
+f1 = input("Enter 1 fruit name: ")
+fruits.append(f1)
+f2 = input("Enter 2 fruit name: ")
+fruits.append(f2)
+f3 = input("Enter 3 fruit name: ")
+fruits.append(f3)
+f4 = input("Enter 4 fruit name: ")
+fruits.append(f4)
+f5 = input("Enter 5 fruit name: ")
+fruits.append(f5)
+f6 = input("Enter 6 fruit name: ")
+fruits.append(f6)
+
+fruits_tuple = tuple(fruits)
+print(fruits_tuple)
+
