38b59b847e11e0e6c3acfc0f9d3e089eebd1c8d7
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-19 21:04:45 +0530

    Added code to input 6 student marks and display them in sorted order

---
+++ chapter 4 Problem set/02_problem2.py
@@ -0,0 +1,17 @@
+# wap to input 6 student to show marks of 6 students in a sorted order
+
+marks = []
+s1 = input("Enter 1 student marks:")
+marks.append(s1)
+s2 = input("Enter 2 student marks:")
+marks.append(s2)
+s3 = input("Enter 3 student marks:")
+marks.append(s3)
+s4 = input("Enter 4 student marks:")
+marks.append(s4)
+s5 = input("Enter 5 student marks:")
+marks.append(s5)
+s6 = input("Enter 6 student marks:")
+marks.append(s6)
+marks.sort()
+print(marks)
