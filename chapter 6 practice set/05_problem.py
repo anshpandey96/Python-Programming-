b1c6986f3eb5c827447d4aab3ac14d074b63f73e
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-21 15:05:52 +0530

    To check the list name exist or not

---
+++ chapter 6 practice set/05_problem.py
@@ -0,0 +1,9 @@
+l = ["ansh","atul","adarsh","divya"]
+
+name = input("Enter your name : ")
+
+if(name in l):
+    print("Your name is in the list")
+else:
+    print("Your name is not in the list")
+
