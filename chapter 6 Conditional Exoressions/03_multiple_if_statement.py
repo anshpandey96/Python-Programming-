263874fea19524edcef738ef42894a23c194ff6b
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-21 13:31:55 +0530

    Multiple if else ladder

---
+++ chapter 6 Conditional Exoressions/03_multiple_if_statement.py
@@ -0,0 +1,23 @@
+a = int (input("Enter you age: "))
+
+# If statement no: 1
+if(a%2== 0):
+    print("a is even")
+
+# If statement no: 1
+
+
+# If statement no:2
+if(a>=18):
+    print("You are above the age of concent")
+
+elif(a<0):
+    print("You are entering an invalid age")
+
+elif(a==0):
+    print("You are  entering 0 which is not a valid age: ")
+
+else:
+    print("you are below the age of concent")
+
+print("End of program")
