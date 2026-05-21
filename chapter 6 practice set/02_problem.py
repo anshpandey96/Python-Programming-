39ec23e7a6effe6e4ae43baa5bd860ed75e45b73
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-21 14:33:48 +0530

    To Check the student pass or fail

--- chapter 6 practice set/02_problem.py
+++ chapter 6 practice set/02_problem.py
@@ -4,8 +4,8 @@ marks3 = int(input("Enter  the marks subject 3:"))
 
 total_percentage =  (100*(marks1+marks2+marks3))/300
 
-if(total_percentage>= 40 ):
-    print("You are pass ")
+if(total_percentage>= 40 and marks3>33 and marks2>33):
+    print("You are pass ",total_percentage)
 
 else:
-    print("You failed, try again next year!")
+    print("You failed, try again next year!",total_percentage)
