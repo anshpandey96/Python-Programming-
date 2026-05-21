ae6a87623a6f66b57311a3848995272a2e4c128d
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-21 13:05:11 +0530

    If else statement

--- chapter 6 Conditional Exoressions/01_conditionals.py
+++ chapter 6 Conditional Exoressions/01_conditionals.py
@@ -1,15 +1,12 @@
 a = int (input("Enter you age: "))
 
+# If else statement
 if(a>=18):
     print("You are above the age of concent")
-
-elif(a<0):
-    print("You are entering an invalid age")
-
-elif(a==0):
-    print("You are  entering 0 which is not a valid age: ")
 
 else:
     print("you are below the age of concent")
 
 print("End of program")
+
+
