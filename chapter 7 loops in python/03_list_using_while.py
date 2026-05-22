5664ac1613d5aeea6237683cec52462f853a123c
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-22 17:09:52 +0530

    While loop in python

---
+++ chapter 7 loops in python/03_list_using_while.py
@@ -0,0 +1,7 @@
+l = [1, "Ansh", "False", "This", "Rohan", "atul"]
+
+i = 0
+
+while(i < len(l)):   # ✅ l (the list), not 1 (the number)
+    print(l[i])
+    i += 1
---
+++ chapter 7 loops in python/04_for_loops.py
@@ -0,0 +1,14 @@
+l = [1,4,6,234,6,764]
+
+for i in l:
+    print(i)
+# for loop with tuples
+
+    t = (6,231,75,122)
+    for i in t:
+        print(i)
+
+#for loop with string
+s = "Ansh"
+for i in t:
+    print(i)
