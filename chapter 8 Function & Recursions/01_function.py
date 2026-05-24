1749cbbd48b3f83e0db7369834def187f73d0157
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-24 23:49:20 +0530

    Introduction to functions

---
+++ chapter 8 Function & Recursions/01_function.py
@@ -0,0 +1,22 @@
+# a = 12
+# b = 45
+# c = 56
+
+# average = ( a + b + c)/3
+# print(average)
+
+
+# a = int(input("Enter your first number: "))
+# b = int(input("Enter your second number: "))
+# c = int(input("Enter your third number: "))
+
+# average = ( a +b +c )/3
+# print(average)
+
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+c = int(input("Enter your third number: "))
+
+average  = ( a  + b + c )/3
+print(average)
+
