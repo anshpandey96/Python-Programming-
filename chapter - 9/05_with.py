097608a8e9ef7396a9a8f009be81eb5a5e94e1d6
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-28 23:06:28 +0530

    With statement for file handling

--- chapter - 9/05_with.py
+++ chapter - 9/05_with.py
@@ -10,3 +10,4 @@ with open("file.txt") as f:
     print(f.read())
 
 # you dont have to explicitly close the file when you use with statement, it will automatically close the file after the block of code is executed. 
+
